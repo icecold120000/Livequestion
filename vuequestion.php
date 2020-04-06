4a7618d0bb935ceec16df4738ed3e65479d5a596
icecold120000 <61144437+icecold120000@users.noreply.github.com>
2020-04-06 17:29:20 +0200

    Création de page vue question

---
+++ vuequestion.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" type="text/css" href="question.css">
+<?php
+	require_once('./Includes/header.php');
+?>
+	<main>
+		<div id="lesquestions">
+			<div>
+				<h1>How are you doing</h1>
+			</div>
+			<div>
+				<span href="#">icecold120000</span>				
+				<span>20 <i class="fas fa-thumbs-up"></i></span>
+				<span href="#">30 reponses <i class="fas fa-caret-down"></i></span>
+			</div>			
+		</div>
+		<div id="lesreponses">
+			<div>
+				<h4>I'm fine</h4>
+			</div>
+			<div>
+				<span>20 <i class="fas fa-thumbs-up"></i></span>
+				<span href="#">Fuzzy2301</span>
+			</div>			
+		</div>
+	</main>
+	<?php
+		require_once('./Includes/footer.php');
+	?>
