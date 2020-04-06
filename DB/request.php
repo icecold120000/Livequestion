2bbc29a4d0c8143b38d72ed1a9ce1f91c1cbe01c
icecold120000 <61144437+icecold120000@users.noreply.github.com>
2020-04-06 15:39:43 +0200

    fonction ajout
    tableau affiché contenant les données de la base de donnée

--- DB/request.php
+++ DB/request.php
@@ -10,7 +10,33 @@
 		return $req;
 	}
 
-	function ajout(array $donnees) {
+	function ajout(array $affiche){
+		
+		$date =[
+			'id_date' => 'son_id_d',
+			'creation_date' =>'sa_date_creation',
+		];
 
+		$utilisateur =[
+			'id_utilisateur' => 'son_id_u',
+			'pseudo_utilisa$teur' =>'son_pseudo',
+			'email_utilisateur' =>'son_email',
+		];
+
+		$question =[
+			'id_question' => 'son_id_q',
+			'titre_question' => 'son_titre',
+			'nombre_likes' => 'ses_likes',
+			"#id_utilisateur" => 'son_user',
+			"#id_date" => 'sa_date',
+		];
+
+		$reponse =[
+			'id_reponse' => 'son_id_r',
+			'contenu_reponse' => 'son_contenu',
+			"#id_question" => 'son_id_q',
+			"#id_utilisateur" => 'son_user',
+			"#id_date" => 'sa_date',
+		];
 	}
 ?>
