8191958fbd9a361a7cdafedc7361f155814b5c50
icecold120000 <61144437+icecold120000@users.noreply.github.com>
2020-04-27 15:54:03 +0200

    Ajout Vue question

---
+++ VueQuestion/vuequestion.php
@@ -0,0 +1,51 @@
+<link rel="stylesheet" type="text/css" href="question.css">
+<?php
+	require_once('./Includes/header.php');
+?>
+	<main>
+		<section id="left" class="container-fluid">
+			<div id="lesquestions" >
+				<div>
+					<h1>How are you doing</h1>
+				</div>
+				<div>
+					<span href="#">icecold120000</span>				
+					<span>20 <i class="fas fa-thumbs-up"></i></span>
+					<span href="#">30 reponses <i class="fas fa-caret-down"></i></span>
+				</div>			
+			</div>
+			<div id="lesreponses" class="container-fluid">
+				<div>
+					<h4>I'm fine</h4>
+				</div>
+				<div>
+					<span>20 <i class="fas fa-thumbs-up"></i></span>
+					<span href="#">Fuzzy2301</span>
+				</div>			
+			</div>
+		</section>
+		<section id="right" class="container-fluid">
+			<div class="list-group">
+				<button type="button" class="list-group-item list-group-item-action active">How are you doing ?
+			  	</button>
+			  	<button type="button" class="list-group-item list-group-item-action">What are you doing ?</button>
+			  	<button type="button" class="list-group-item list-group-item-action">Where are you ?</button>
+			  	<button type="button" class="list-group-item list-group-item-action">Who are you ?</button>
+			  	<button type="button" class="list-group-item list-group-item-action">When are you coming back ?</button>
+			</div>
+		</section>
+		<section class="form">
+			<form method="POST" action=""> 
+				<div class="container-fluid">
+					<div class="form-group">
+						<label class="info" for="question">Soumettre une reponse:</label>
+						<textarea class="form-control" cols="50" rows="20" placeholder="Veuiller écrire votre question."></textarea>
+						<button type="submit" class="btn btn-success">Valider</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</main>
+	<?php
+		require_once('./Includes/footer.php');
+	?>
