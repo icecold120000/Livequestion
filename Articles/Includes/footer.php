8fa10ebb126a880c4a58b2d634165a07a8b5bf59
Arthur <valentin.arthur1000@gmail.com>
2020-04-30 12:15:08 +0200

    Articles

---
+++ Articles/Includes/footer.php
@@ -0,0 +1,7 @@
+		<footer>
+			<link rel="stylesheet" type="text/css" href="style.css">
+			<p>&copy;Page protected by reCAPTCHA and subject to Google's Privacy Policy and Terms of Service</p>
+			<img src="Contents/links.jpg">
+		</footer>
+	</body>
+</html>
---
+++ Articles/index3.php
@@ -0,0 +1,36 @@
+<!-- Haut de page -->
+<?php
+	require_once("Includes/head.php");
+?>
+<!-- Menu -->
+<div class="black">
+	<div>
+		<img src="Contents/logopage.png"><a href="" class="menu">Répondre aux questions</a><a href="" class="menu">Communauté</a> <a href="index.php" class="menu">Article 1</a> <a href="index2.php" class="menu">Article 2</a> <a href="index3" class="menu">Article 3</a>
+	</div>
+	<!-- Profil -->
+	<p class="texte"><img src="Contents/Upload_Profile/persona1.jpg" class="profile" />France, Nourriture, alimentation, 1 critère de ciblage [100 avis sur 100 attendus | Terminé]</p>
+	<!-- Commentaire -->
+	<div class="comments">
+		<aside class="right">
+			<section class="Comments1">
+				<div class="commentimg">
+					<img class="CommentIcon" src="Contents/Upload_Profile/persona3.jpg">
+				</div>
+				<div class="commentp">
+					<p><strong>Praesent vitae velit tristique old alos</strong></p>
+					<p>Ned ut perspiciatias unde omnis ise natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa</p>
+					<p>"Proin vel dolor dictum, congue tellus at, lobortis neque"</p>
+				</div>
+			</section>
+		</aside>
+	</div>
+	<!-- Grapgiques -->
+	<div>
+		<img src="Contents/nourriture.jpg" class="nourriture">
+		<img src="Contents/anneaux.png" class="anneaux">
+	</div>
+	<div>
+		<h3>Qui sont les membres qui ont répondu ?</h3>
+		<img src="Contents/graphique.png" class="graphique">
+	</div>
+</div>
