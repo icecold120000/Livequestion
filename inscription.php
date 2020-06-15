7a11df814b01a9207b3b4376762aadde9ccf38bb
icecold120000 <61144437+icecold120000@users.noreply.github.com>
2020-06-15 11:29:24 +0200

    php fix

--- inscription.php
+++ inscription.php
@@ -5,9 +5,10 @@
 	if (!empty($_POST)) {
 		$traitement = preTraitement($_POST);
 		if($traitement['success']){
-			Request($DB, "INSERT user FROM utilisateur",$pseudo_utilisateur);
-			Request($DB, "INSERT email FROM utilisateur",$email_utilisateur);
-			Request($DB, "INSERT mdp FROM utilisateur",$mdp_utilisateur);			
+			Request($DB, "INSERT :user INTO utilisateur",$_POST['pseudo']);
+			Request($DB, "INSERT :email INTO utilisateur",$_POST['email']);
+			Request($DB, "INSERT :genre INTO utilisateur",$_POST['genre']);
+			Request($DB, "INSERT :mdp INTO utilisateur",$_POST['mdp']);			
 		}
 	}
 ?>
@@ -15,11 +16,11 @@
 		<div class="retour">
 			<a href="index.php"><button class="btn btn-success">Retourner à la page d'acceuil</button></a>
 		</div>
-		<form action="#" method="POST">
+		<form action="" method="POST">
 			<div class="bordure">
 				<div class="saisie">
-					<label for="nom">Nom de l'utilisateur : </label>				
-					<input type="text" name="user" id="nom" placeholder="Utilisateur" />
+					<label for="nom">Nom de l'utilisateur:</label>				
+					<input type="text" name="user" id="nom" placeholder="Veuillez saisir votre utilisateur:" />
 					<?php
 						if (isset($traitement['erreurs']['user'])) {
 							echo "<span class='erreur'>".$traitement['erreurs']['user']."</span>";
@@ -27,8 +28,8 @@
 					?>
 				</div>
 				<div class="saisie">
-					<label for="email">Email : </label>
-					<input type="text" name="email" id="email" placeholder="Email" />
+					<label for="email">Email:</label>
+					<input type="text" name="email" id="email" placeholder="Veuillez saisir votre émail:" />
 					<?php
 						if (isset($traitement['erreurs']['email'])) {
 							echo "<span class='erreur'>".$traitement['erreurs']['email']."</span>";
@@ -36,15 +37,20 @@
 					?>
 				</div>
 				<div class="saisie">
-					<label for="genre">Genre : </label>
-					<select id="genre">
-						<option value="valeur1" selected>Homme</option>
+					<label for="genre">Genre:</label>
+					<select id="genre" name="genre">
+						<option value="valeur1">Homme</option>
 						<option value="valeur2">Femme</option>
 					</select>
+					<?php
+						if (isset($traitement['erreurs']['genre'])) {
+							echo "<span class='erreur'>".$traitement['erreurs']['genre']."</span>";
+						}
+					?>
 				</div>
 				<div class="saisie">
-					<label for="mdp">Mot de passe : </label>
-					<input type="password" name="mdp" id="mdp" placeholder="Mot de passe" />
+					<label for="mdp">Mot de passe:</label>
+					<input type="password" name="mdp" id="mdp" placeholder="Veuillez saisir votre mot de passe:" />
 					<?php
 						if (isset($traitement['erreurs']['mdp'])) {
 							echo "<span class='erreur'>".$traitement['erreurs']['mdp']."</span>";
@@ -52,15 +58,12 @@
 					?>
 				</div>
 				<div class="submit">
-					<input type="submit" value="S'inscrire" class="btn btn-primary">
+					<input type="submit" value="Valider" class="btn btn-primary">
 				</div>
 			</div>
 		</form>
 		<div class="more">
-			<p>Déjà inscrit ?<a href="formconnexion.php">
-			<div>
-				Appuyez ici pour vous connecter.</a></p>
-			</div> 
+			<p>Déjà inscrit? <a href="formconnexion.php">Appuyer ici pour vous connecter.</a></p>
 		</div>
 	</main>
 <?php
