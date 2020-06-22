29d11e9d0fb1db44bd3954949aa5baecd22a815c
icecold120000 <61144437+icecold120000@users.noreply.github.com>
2020-06-22 09:16:56 +0200

    ajout du traitement

--- userpage.php
+++ userpage.php
@@ -1,13 +1,13 @@
 ﻿<?php
 	require_once('./Includes/header.php');
-	require_once('./Traitement/traitement_inscription.php');
+	require_once('./Traitement/traitement_userpage.php');
 	
 	if (!empty($_POST)) {
 		$traitement = preTraitement($_POST);
 		if($traitement['success']){
 			Request($DB, "UPDATE :nameuser INTO utilisateur",$_POST['nameuser']);
 			Request($DB, "UPDATE :emailuser INTO utilisateur",$_POST['emailuser']);
-			Request($DB, "UPDATE :passworduser INTO utilisateur",$_POST['passworduser']);			
+			Request($DB, "UPDATE :mdpuser INTO utilisateur",$_POST['mdpuser']);			
 		}
 	}
 ?>
@@ -26,15 +26,30 @@
 			<div class="formulaire" method="POST">
 				<div>
 					<span>Nom d'utilisateur *</span>
-					<input type="text" name="nameuser" placeholder="Votre nom d'utlilisateur" class="input">
+					<input type="text" name="nameuser" placeholder="<?php Request($DB, "INSERT :nameuser FROM utilisateur"); ?>" class="input">
+					<?php
+						if (isset($traitement['erreurs']['nameuser'])) {
+							echo "<span class='erreur'>".$traitement['erreurs']['nameuser']."</span>";
+						}
+					?>
 				</div>
 				<div>
 					<span>E-mail *</span>
-					<input type="text" name="emailuser" placeholder="Votre E-mail" class="input">
+					<input type="text" name="emailuser" placeholder="<?php Request($DB, "INSERT :emailuser FROM utilisateur"); ?>" class="input">
+					<?php
+						if (isset($traitement['erreurs']['emailuser'])) {
+							echo "<span class='erreur'>".$traitement['erreurs']['emailuser']."</span>";
+						}
+					?>
 				</div>
 				<div>
 					<span>Mot de passe *</span>
-					<input type="text" name="passworduser" placeholder="Votre mot de passe" class="input">
+					<input type="text" name="mdpuser" placeholder="<?php Request($DB, "INSERT :mdpuser FROM utilisateur"); ?>" class="input">
+					<?php
+						if (isset($traitement['erreurs']['mdpuser'])) {
+							echo "<span class='erreur'>".$traitement['erreurs']['mdpuser']."</span>";
+						}
+					?>
 				</div>
 				<div>
 					<input type="submit" name="submit" placeholder="Valider" class="bouttonsubmit">
